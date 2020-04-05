a302f12a776d3730ea6e420b2ecc5aa8fbd00d87
zhongguoqing <zgqingmail@163.com>
2020-04-05 18:21:11 +0800

    项目内部使用Disruptor做消息队列

---
+++ src/main/java/com/example/utils/demo/disrupMq/service/DisruptorMqService.java
@@ -0,0 +1,10 @@
+package com.example.utils.demo.disrupMq.service;
+
+public interface DisruptorMqService {
+
+    /**
+     * 消息
+     * @param message
+     */
+    void sayHelloMq(String message);
+}
