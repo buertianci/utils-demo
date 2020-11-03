120c21bb14c4e5afd9ebd2298a3bc38ea8222990
zhongguoqing <zgqingmail@163.com>
2020-11-03 23:28:51 +0800

    会有多少车队到达目的地算法

--- src/main/java/com/example/utils/demo/util/FunctionDemoUtil.java
+++ src/main/java/com/example/utils/demo/util/FunctionDemoUtil.java
@@ -80,6 +80,59 @@ public class FunctionDemoUtil {
         }
     }
 
+    /**
+     * 题目描述： N 辆车沿着一条车道驶向位于 target 英里之外的共同目的地。
+     * 每辆车 i 以恒定的速度 speed[i] （英里/小时），从初始位置 position[i] （英里） 沿车道驶向目的地。
+     * 一辆车永远不会超过前面的另一辆车，但它可以追上去，并与前车以相同的速度紧接着行驶。
+     * 此时，我们会忽略这两辆车之间的距离，也就是说，它们被假定处于相同的位置。
+     * 车队 是一些由行驶在相同位置、具有相同速度的车组成的非空集合。
+     * 注意，一辆车也可以是一个车队。即便一辆车在目的地才赶上了一个车队，它们仍然会被视作是同一个车队。
+     * 输入：target = 12 position = [10,8,0,5,3] speed = [2,4,1,1,3]
+     * 输出：3
+     * @param target
+     * @param position
+     * @param speed
+     * @return
+     */
+    public static int carTeam(int target, int[] position, int[] speed) {
+
+        //返回车队数量
+        int carTeamNum = 0;
+        if (position.length == 0) {
+            return 0;
+        }
+
+        //便于计算先排个序-位置
+        for (int i=0; i<position.length-1; i++) {
+            for (int j=i + 1; j<position.length-i-1; j++) {
+                if (position[j+1] > position[j]) {
+                    int temp = position[j];
+                    position[j] = position[j+1];
+                    position[j+1] = temp;
+                }
+            }
+        }
+
+        double[] time = new double[position.length];
+
+        //计算每辆车速度
+        for (int i=0; i<position.length; i++) {
+            time[i] = (target - position[i]) / (speed[i]*1.0);
+        }
+
+        double firstCarTime = time[0];
+        for (int i=0; i<time.length;) {
+            if (firstCarTime >= time[i]) {
+                i ++;
+            } else {
+                carTeamNum++;
+                firstCarTime = time[i++];
+            }
+        }
+        carTeamNum++ ;
+        return carTeamNum;
+    }
+
     public static void main(String[] args) {
         //reserve方法
         System.out.println("reserve方法：    "+ReverseString.reserveStrByStringBuilder("abcdef"));
@@ -89,5 +142,9 @@ public class FunctionDemoUtil {
         System.out.println("字符串数组方法：   "+ReverseString.reverseStrBySelf("abcdef"));
         //Collections实现
         System.out.println("Collections方法： "+ReverseString.reverseStrByCollection("abcdef"));
+        //会有多少车队到达目的地
+        int[] position = new int[]{10, 8, 0, 5 ,3};
+        int[] speed = new int[]{2, 4, 1, 1, 3};
+        System.out.println(carTeam(12, position,speed));
     }
 }
