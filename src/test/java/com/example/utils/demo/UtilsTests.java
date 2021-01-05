3dce402eee78c4ad5ce451b32ff2e1bef4c79c90
zhongguoqing <zgqingmail@163.com>
2021-01-05 23:08:42 +0800

    新增http请求工具类

---
+++ src/test/java/com/example/utils/demo/UtilsTests.java
@@ -0,0 +1,94 @@
+package com.example.utils.demo;
+
+import com.example.utils.demo.util.EncryptionUtil;
+import com.example.utils.demo.util.HttpClientUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DemoApplication.class)
+public class UtilsTests {
+
+    @Test
+    public void test1() {
+        System.out.print("Hello world!");
+    }
+
+    /**
+     * base64加密
+     */
+    @Test
+    public void encryptBASE64Test() {
+        try {
+            String key = EncryptionUtil.encryptBASE64("小红".getBytes("UTF-8"));
+            System.out.print(key);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * base64解密
+     */
+    @Test
+    public void decryptBASE64StrTest() {
+
+        try {
+            String value = EncryptionUtil.decryptBASE64Str("5bCP57qi");
+            System.out.print(value);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * get请求
+     */
+    @Test
+    public void sendHttpGetTest() {
+        String url = "https://postman-echo.com/get?test=123";
+        String result = HttpClientUtil.sendHttpGet(url);
+        System.out.print(result);
+    }
+
+    /**
+     * POST 请求
+     * 获取access_token
+     */
+    @Test
+    public void getAccessTokenTest() throws Exception{
+        String url = "https://account-gyd.glodon.com/v3/api/oauth2/token";
+        String appKey = "QSjJvHXvYYY4mjqrwbIXmOUshya9Qf5i";
+        String appSecret = "nOv18VCOSpLCb01SfPtKAgSyzDl8hPui";
+        String creds = String.format("%s:%s", appKey, appSecret);
+        Map<String,String> contentMap = new HashMap<>();
+        contentMap.put("grant_type", "client_credentials");
+        String result = HttpClientUtil.sendPostFormData(url, contentMap, "Basic " + EncryptionUtil.encryptBASE64(creds));
+        System.out.print(result);
+    }
+
+    /**
+     * POST 请求
+     * check token
+     */
+    @Test
+    public void checkTokenTest() throws Exception{
+        String url = "https://account-gyd.glodon.com/v3/api/oauth2/check_token";
+        String appKey = "df9mCsrbLmAuSiiISs0jQhIeI5Ji9UnD";
+        String appSecret = "JqRCaE8UTGkeL3btsCqvf4Pvli6jU8Rr";
+        String creds = String.format("%s:%s", appKey, appSecret);
+        Map<String,String> contentMap = new HashMap<>();
+        contentMap.put("token", "cn-1b7a72a3-877a-4d5a-ae73-cff1a7a362b4");
+        String result = HttpClientUtil.sendPostFormData(url, contentMap, "Basic " + EncryptionUtil.encryptBASE64(creds));
+        System.out.print(result);
+    }
+
+}
