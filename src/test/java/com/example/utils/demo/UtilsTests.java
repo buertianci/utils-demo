4c5370ac562afd94ba37782b9660d922787356b8
zhongguoqing <zgqingmail@163.com>
2021-03-18 17:45:02 +0800

    新增JsonUtil转换工具类

--- src/test/java/com/example/utils/demo/UtilsTests.java
+++ src/test/java/com/example/utils/demo/UtilsTests.java
@@ -1,7 +1,10 @@
 package com.example.utils.demo;
 
+import com.alibaba.fastjson.JSON;
 import com.example.utils.demo.util.EncryptionUtil;
 import com.example.utils.demo.util.HttpClientUtil;
+import com.example.utils.demo.util.JsonUtils;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -91,4 +94,25 @@ public class UtilsTests {
         System.out.print(result);
     }
 
+    @Test
+    public void tranTuoFengAndXiahuaxian() throws Exception{
+        User user = new User();
+        user.setUserName("小明");
+        user.setUserEmail("123@163.com");
+        user.setUserAge(18);
+        //驼峰--->下划线
+        String jsonStr = JsonUtils.toUnderlineJSONString(user);
+        System.out.println("驼峰转换成下划线：" + jsonStr);
+        //下划线-->驼峰
+        User user1 = JsonUtils.toSnakeObject(jsonStr, User.class);
+        System.out.println("下划线转换成驼峰形式：" + JSON.toJSONString(user1));
+    }
+
+}
+
+@Data
+class User {
+    private String userName;
+    private String userEmail;
+    private int userAge;
 }
